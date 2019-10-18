c75dff6c336f03a6cad51cc61d39205701fac583
Marcus <marcus68@protonmail.com>
2019-10-18 15:22:23 -0700

    Finished 2 sql scripts and java/html files

--- PokeDB/src/Pokemon.java
+++ PokeDB/src/Pokemon.java
@@ -23,9 +23,6 @@ public class Pokemon extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		// SQL statements
-		String sql = "SELECT number, name, type, hp, attack, defense from pokemon where name = ?";
-
 		response.setContentType("text/html"); // Set response content type
 		PrintWriter out = response.getWriter();
 
@@ -33,33 +30,80 @@ public class Pokemon extends HttpServlet {
 			
 			// get input data from form
 			String name = request.getParameter("name");
-			
-			// get data from form and convert to integer values
+			String type = request.getParameter("type");
+			if (name.length() > 0)
+			{
+				String sql = "SELECT p.name, p.type, p.hp, p.attack, p.defense, m.primary_attack, m.secondary_attack\n" + 
+						"FROM pokedex.pokemon p\n" + 
+						"	JOIN pokedex.moveset m\n" + 
+						"		ON p.number = m.number\n" + 
+						"WHERE p.name = ?";
+				PreparedStatement pstmt =  conn.prepareStatement(sql);
+				pstmt.setString(1, name);
+				ResultSet rs = pstmt.executeQuery();
 
-			// prepare sql select
-			PreparedStatement pstmt =  conn.prepareStatement(sql);
-			pstmt.setString(1, name);
-			ResultSet rs = pstmt.executeQuery();
-
-			out.println("<!DOCTYPE HTML><html><body>");
-			out.println("<img src=\"Pokemon.jpeg\" height=\"137\" width=\"368\" />");
-			out.println("<table border = \"1\" width = \"100%\">");
-			out.println("<col style = \"width:16%\"> <col style = \"width:16%\"> <col style = \"width:16%\">"
-					+ "<col style = \"width:16%\"> <col style = \"width:16%\"> <col style = \"width:16%\">");
-			out.println("<thead><tr><th>Number</th><th>Name</th> <th>Type</th> <th>HP</th> <th>Attack</th> <th>Defense</th></tr></thead>");
-			while (rs.next()) {
-				out.println("<tbody><tr>");
-				out.println("<td>" + rs.getInt("number") + "</td>");
-				out.println("<td>" + rs.getString("name") + "</td>");
-				out.println("<td>" + rs.getString("type") + "</td>");
-				out.println("<td>" + rs.getInt("hp") + "</td>");
-				out.println("<td>" + rs.getInt("attack") + "</td>");
-				out.println("<td>" + rs.getInt("defense") + "</td>");
-				out.println("</tr><tbody>");
+				out.println("<!DOCTYPE HTML><html><body>");
+				out.println("<img src=\"Pokemon.jpeg\" height=\"137\" width=\"368\" />");
+				out.println("<table border = \"1\" width = \"100%\">");
+				out.println("<col style = \"width:14%\"> <col style = \"width:14%\"> <col style = \"width:14%\">"
+						+ "<col style = \"width:14%\"> <col style = \"width:14%\"> <col style = \"width:14%\"><col style = \"width:14%\">");
+				out.println("<thead><tr><th>Name</th> <th>Type</th> <th>HP</th> <th>Attack</th> <th>Defense</th><th>Primary Attack</th><th>Secondary Attack</th></tr></thead>");
+				while (rs.next()) 
+				{
+					out.println("<tbody><tr>");
+					out.println("<td>" + rs.getString("name") + "</td>");
+					out.println("<td>" + rs.getString("type") + "</td>");
+					out.println("<td>" + rs.getInt("hp") + "</td>");
+					out.println("<td>" + rs.getInt("attack") + "</td>");
+					out.println("<td>" + rs.getInt("defense") + "</td>");
+					out.println("<td>" + rs.getString("primary_attack") + "</td>");
+					out.println("<td>" + rs.getString("secondary_attack") + "</td>");
+					out.println("</tr><tbody>");
+				}
+				rs.close();
+				
+				out.println("</table>");
+				out.println("</body></html>");
 			}
-			rs.close();
-			out.println("</table>");
-			out.println("</body></html>");
+			
+			else if (type.length() > 0)
+			{
+				String sql = "SELECT p.name, m.primary_attack, m.secondary_attack\n" + 
+						"FROM pokedex.moveset m\n" + 
+						"	JOIN pokedex.pokemon p\n" + 
+						"		ON m.number = p.number\n" + 
+						"WHERE \n" + 
+						"	primary_attack in \n" + 
+						"		(SELECT name\n" + 
+						"		FROM pokedex.attack\n" + 
+						"		WHERE type = ?) OR\n" + 
+						"	secondary_attack IN\n" + 
+						"		(SELECT name\n" + 
+						"		FROM pokedex.attack\n" + 
+						"		WHERE type = ?)";
+				PreparedStatement pstmt =  conn.prepareStatement(sql);
+				pstmt.setString(1, type);
+				pstmt.setString(2, type);
+				ResultSet rs = pstmt.executeQuery();
+
+				out.println("<!DOCTYPE HTML><html><body>");
+				out.println("<img src=\"Pokemon.jpeg\" height=\"137\" width=\"368\" />");
+				out.println("<table border = \"1\" width = \"100%\">");
+				out.println("<col style = \"width:14%\"> <col style = \"width:14%\"> <col style = \"width:14%\">");
+				out.println("<thead><tr><th>Name</th><th>Primary Attack</th><th>Secondary Attack</th></tr></thead>");
+				while (rs.next()) 
+				{
+					out.println("<tbody><tr>");
+					out.println("<td>" + rs.getString("name") + "</td>");
+					out.println("<td>" + rs.getString("primary_attack") + "</td>");
+					out.println("<td>" + rs.getString("secondary_attack") + "</td>");
+					out.println("</tr><tbody>");
+				}
+				rs.close();
+				out.println("</table>");
+				out.println("</body></html>");
+			}
+			
 		} catch (SQLException e) {
 			// Handle errors
 			e.printStackTrace();
